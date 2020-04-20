9f691bdeef0075aa0397d4a58df10c94dbc4074b
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-04-20 18:27:37 +0200

    chore(setup): moving devDependancy configs outside of the package.json

---
+++ .jestrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  verbose: true,
+  roots: [
+    "src",
+    "tests"
+  ],
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+}
+
---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "*.{js,ts,tsx}": [
+    "eslint --fix"
+  ]
+};
+
+
