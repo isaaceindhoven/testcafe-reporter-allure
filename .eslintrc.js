af564269acdc69d98783decce39c1f3dd860fcf3
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-04-20 17:20:08 +0200

    chore(setup): fixing lint command to use gitignore and cover config files, split ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: ['./tsconfig.json'],
-    },
-    extends: [
-        'airbnb-typescript/base',
-        'plugin:prettier/recommended',
-        'plugin:jest/recommended',
-    ],
-    rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    }
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.test.json'],
+  },
+  extends: ['airbnb-typescript/base', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
 };
