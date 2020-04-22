e3e5b79c6e88ca6a60e138c1cd34002acef6e224
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-04-22 11:11:24 +0200

    chore(testcafe): adding e2e tests to the husky pre-commit tests

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   hooks: {
-    'pre-commit': 'npm run build:prod && npm run test:unit && lint-staged',
+    // Pre-commit: Test if production build works, run unit and e2e tests, run lint-staged.
+    'pre-commit': 'npm run build:prod && npm run test:unit && npm run test:e2e && lint-staged',
     'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
   },
 };
