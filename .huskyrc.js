b079a5bc4d580c2c6158b34ac832c60790fd0f60
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-04-23 17:36:03 +0200

    chore(testcafe): removing e2e tests from the git commit hook

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   hooks: {
     // Pre-commit: Test if production build works, run unit and e2e tests, run lint-staged.
-    'pre-commit': 'npm run build:prod && npm run test:unit && npm run test:e2e && lint-staged',
+    'pre-commit': 'npm run build:prod && npm run test:unit && lint-staged',
     'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
   },
 };
