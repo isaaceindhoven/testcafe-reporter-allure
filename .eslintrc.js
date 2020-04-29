33060af595a212a7581acc927fa93e6412ee978b
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-04-29 17:16:33 +0200

    chore(linting): whitelisting linting plugins by making use of cascading linting configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,5 @@ module.exports = {
   parserOptions: {
     project: ['./tsconfig.test.json'],
   },
-  extends: ['airbnb-typescript/base', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
-  overrides: [
-    {
-      files: ['**/e2e/**'],
-      rules: {
-        'jest/expect-expect': 'off',
-        'jest/no-test-callback': 'off',
-      },
-    },
-  ],
+  extends: ['airbnb-typescript/base', 'plugin:prettier/recommended'],
 };
