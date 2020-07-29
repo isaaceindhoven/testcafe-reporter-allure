70057762e64676d41b967436b61deb832fdc50aa
Maarten Van Hoof <maarten.vanhoof@isaac.nl>
2020-07-29 17:36:06 +0200

    chore(husky): remove unused pre-commit comment

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   hooks: {
-    // Pre-commit: Test if production build works, run unit and run lint-staged.
     'pre-commit': 'lint-staged',
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
