98fcfcc832a6dd415bfdb9374e1f985d18c0f7de
Maarten Van Hoof <maarten.vanhoof@isaac.nl>
2020-07-29 17:34:12 +0200

    build(commitlint): use commitlint in favor of commitizen

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,6 +2,6 @@ module.exports = {
   hooks: {
     // Pre-commit: Test if production build works, run unit and run lint-staged.
     'pre-commit': 'npm run build && npm run test:unit && lint-staged',
-    'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };
